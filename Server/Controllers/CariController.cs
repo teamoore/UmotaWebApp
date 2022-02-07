1bf95a5cb99f9e7e75caaedcf7ec3a819c8b25c0
Koray <koray_taylan@hotmail.com>
2022-02-07 18:32:19 +0300

    Cari Controller ekledim.

---
+++ Server/Controllers/CariController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UmotaWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CariController : ControllerBase
+    {
+        public ILogger<CariController> Logger { get; }
+
+        public CariController(ILogger<CariController> logger)
+        {
+            Logger = logger;
+        }
+
+        [HttpGet("list")]
+        public string MyCode()
+        {
+            return "Koray"; 
+        }
+    }
+}
