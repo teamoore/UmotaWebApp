a46388d7231e3090229885eb27110e08aebca196
TIMUR.GUNDOGDU <timurgundogdu@gmail.com>
2022-02-09 05:15:29 +0300

    cari kart servisi yazıldı

---
+++ Server/Services/Infrastructure/CariKartService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UmotaWebApp.Server.Data.Models;
+using UmotaWebApp.Shared.ModelDto;
+
+namespace UmotaWebApp.Server.Services.Infrastructure
+{
+
+    public class CariKartService : ICariKartService
+    {
+        public IMapper Mapper { get; }
+        public UmotaCompanyDbContext Db { get; }
+        public IConfiguration Configuration { get; }
+
+        public CariKartService(IConfiguration configuration, UmotaCompanyDbContext db, IMapper mapper)
+        {
+            Configuration = configuration;
+            Db = db;
+            Mapper = mapper;
+        }
+
+        public async Task<List<CariKartDto>> GetCariKartDtos()
+        {
+            var results = await Db.CariKarts.ProjectTo<CariKartDto>(Mapper.ConfigurationProvider).ToListAsync();
+
+            return results;
+        }
+    }
+}
--- Server/Services/Infrastructure/ICariKartService.cs
+++ Server/Services/Infrastructure/ICariKartService.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UmotaWebApp.Shared.ModelDto;
 
 namespace UmotaWebApp.Server.Services.Infrastructure
 {
-    public interface ISisCariKartService
+    public interface ICariKartService
     {
-        
+        public Task<List<CariKartDto>> GetCariKartDtos();
     }
 }
