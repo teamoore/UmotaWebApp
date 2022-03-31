054ab4a5723c0dedf0b5c155605e6a225c3416a1
TIMUR.GUNDOGDU <timurgundogdu@gmail.com>
2022-03-31 17:56:17 +0300

    detay ara malzeme

---
+++ Client/Utils/ClientEnums/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UmotaWebApp.Client.Utils.ClientEnums
+{
+    public class Enums
+    {
+        public enum MalzemeDetayliArama
+        {
+            Genel = 0,
+            Marka = 1,
+            Adi = 2
+        }
+    }
+}
