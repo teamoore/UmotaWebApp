0280e481742ecdefb4ead38b2954782ef9cb48a7
Koray <koray_taylan@hotmail.com>
2022-04-05 13:41:24 +0300

    Malzeme Birim Seti

---
+++ Shared/ModelDto/MalzemeBirimSetDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmotaWebApp.Shared.ModelDto
+{
+    public class MalzemeBirimSetDto
+    {
+        public int BirimSetRef { get; set; }
+        public string BirimSetKodu { get; set; }
+        public int AnaBirimRef { get; set; }
+        public string AnaBirimKodu { get; set; }
+    }
+}
