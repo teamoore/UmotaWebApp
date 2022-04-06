301efe4ba4b864eee9cee53fb6a9a29914e31946
Koray <koray_taylan@hotmail.com>
2022-04-06 13:47:15 +0300

    Cari Malzeme Düzenlemeler

--- Shared/ModelDto/VCariKartDto.cs
+++ Shared/ModelDto/VCariKartDto.cs
@@ -6,47 +6,42 @@ using System.Threading.Tasks;
 
 namespace UmotaWebApp.Shared.ModelDto
 {
-    public class CariKartDto
+    public class VCariKartDto
     {
         public int Logref { get; set; }
+        public byte? Caritip { get; set; }
+        public string Caritipadi { get; set; }
         public string Kodu { get; set; }
         public string Adi { get; set; }
         public string Adi2 { get; set; }
         public string Adres1 { get; set; }
         public string Adres2 { get; set; }
-        public int? Semtref { get; set; }
         public string Semt { get; set; }
-        public int? Ilceref { get; set; }
         public string Ilce { get; set; }
-        public int? Ilref { get; set; }
         public string Il { get; set; }
-        public int? Ulkeref { get; set; }
         public string Ulke { get; set; }
+        public int? Semtref { get; set; }
+        public int? Ilceref { get; set; }
+        public int? Ilref { get; set; }
+        public int? Ulkeref { get; set; }
         public string Tel1 { get; set; }
         public string Tel2 { get; set; }
         public string Faks { get; set; }
         public string Mail { get; set; }
         public string Web { get; set; }
-        public string Vdno { get; set; }
-        public string Vdadi { get; set; }
-        public short? Sahismi { get; set; }
         public string Postakodu { get; set; }
+        public string Vdadi { get; set; }
         public string Ozelkod { get; set; }
         public string Ozelkod2 { get; set; }
         public string Ozelkod3 { get; set; }
         public string Ozelkod4 { get; set; }
         public string Ozelkod5 { get; set; }
-        public int? Logoref { get; set; }
-        public string Nakliye { get; set; }
-        public string Nakliyeodeme { get; set; }
-        public int? Odmplnref { get; set; }
-        public byte? Active { get; set; }
-        public byte? Status { get; set; }
-        public string Insuser { get; set; }
-        public DateTime? Insdate { get; set; }
-        public string Upduser { get; set; }
-        public DateTime? Upddate { get; set; }
-        public byte? Caritip { get; set; }
-        public string Caritipadi { get; set; }
+        public int? Odemeplanref { get; set; }
+        public string Odmplnkodu { get; set; }
+        public short? Active { get; set; }
+        public short? Status { get; set; }
+        public short? Isperscomp { get; set; }
+        public string Vdno { get; set; }
+        public string Sektor { get; set; }
     }
 }
