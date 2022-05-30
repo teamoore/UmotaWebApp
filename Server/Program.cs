9468f7bbf603afa34cf724af0baf21d637079697
TIMUR.GUNDOGDU <timurgundogdu@gmail.com>
2022-05-30 08:24:08 +0300

    logger

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UmotaWebApp.Server.Logging;
 
 namespace UmotaWebApp.Server
 {
@@ -13,21 +9,28 @@ namespace UmotaWebApp.Server
     {
         public static void Main(string[] args)
         {
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //    .Build();
+
+            //Log.CloseAndFlush();
+            //Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(i =>
-            {
-                i.ClearProviders();
-                i.SetMinimumLevel(LogLevel.Trace);
-                i.AddDebug();
-                i.AddConsole();
-            })
+                .ConfigureLogging(i =>
+                {
+                    i.ClearProviders();
+                    i.SetMinimumLevel(LogLevel.Trace);
+                    i.AddProvider(new UmotaLoggerProvider());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>();
                 });
     }
 }
