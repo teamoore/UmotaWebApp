f20ee064b203961e3bcb3e6f339d4d2ef8e66dfe
Koray <koray_taylan@hotmail.com>
2022-06-09 12:16:24 +0300

    bug fixed timur

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using UmotaWebApp.Server.Logging;
@@ -9,13 +10,6 @@ namespace UmotaWebApp.Server
     {
         public static void Main(string[] args)
         {
-            //IConfigurationRoot configuration = new ConfigurationBuilder()
-            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //    .Build();
-
-            //Log.CloseAndFlush();
-            //Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
-
             CreateHostBuilder(args).Build().Run();
         }
 
