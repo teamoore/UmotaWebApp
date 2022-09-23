a05ab4efe7091a17255b446f2172384e032a4437
Koray <koray_taylan@hotmail.com>
2022-09-23 15:32:16 +0300

    Excele gönder eksik olanları tamamladım

--- Client/ServiceHelpers/VazifeHelper.cs
+++ Client/ServiceHelpers/VazifeHelper.cs
@@ -80,7 +80,8 @@ namespace UmotaWebApp.Client.ServiceHelpers
             vazife.Insdate = DateTime.Now;
             vazife.Insuser = await LocalStorageService.GetItemAsync<string>(Consts.KullaniciKodu);
             vazife.Status = 0;
-            vazife.SonTarih = vazife.SonTarih.Value.ChangeTime(vazife.SonTarihSaat);
+            if (vazife.SonTarihSaat.HasValue)
+                vazife.SonTarih = vazife.SonTarih.Value.ChangeTime(vazife.SonTarihSaat);
 
             var request = new VazifeRequestDto();
             request.Vazife = vazife;
@@ -111,7 +112,8 @@ namespace UmotaWebApp.Client.ServiceHelpers
             if (selectedFirmaDonem == null)
                 throw new Exception("Firma Dönem Seçili değil");
 
-            vazife.SonTarih = vazife.SonTarih.Value.ChangeTime(vazife.SonTarihSaat);
+            if (vazife.SonTarihSaat.HasValue)
+                vazife.SonTarih = vazife.SonTarih.Value.ChangeTime(vazife.SonTarihSaat);
 
             var request = new VazifeRequestDto();
             request.Vazife = vazife;
