6c749807a53ea9d5ce795cdef34a0c97170e13ee
Koray <koray_taylan@hotmail.com>
2023-08-28 09:59:57 +0300

    sipariş

--- Prizma.Core/Model/Siparis.cs
+++ Prizma.Core/Model/Siparis.cs
@@ -16,16 +16,20 @@ namespace Prizma.Core.Model
         public int CariRef { get; private set; }
         public int ProjeRef { get; private set; }
         public int DurumRef { get; private set; }
-        public string? Aciklama { get; private set; }
         public int? LgFirmaNo { get; private set; }
         public int DovizRefID { get; private set; }
         public int DovizRefRD { get; private set; }
+        public byte DovizKurTurID { get; set; }
         public double DovizKuruID { get; private set; }
         public double DovizKuruRD { get; private set; }
         public double NetToplamID { get; private set; }
         public double NetToplamRD { get; private set; }
         public double NetToplamTL { get; private set; }
-
+        public byte DosyaEki { get; set; }
+        public byte Iptal { get; set; }
+        public string? IptalUser { get; set; }
+        public DateTime? IptalDate { get; set; }
+        public string? IptalNedeni { get; set; }
 
         #endregion
         public Siparis() { }
